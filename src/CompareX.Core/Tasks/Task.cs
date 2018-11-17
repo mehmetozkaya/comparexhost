9a2f3e13ecc19376a986f60f1fe06f770b520fa6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-17 14:06:06 +0300

    task entity created

--- src/CompareX.Core/Tasks/Task.cs
+++ src/CompareX.Core/Tasks/Task.cs
@@ -1,7 +1,9 @@
 ﻿using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -13,7 +15,34 @@ namespace CompareX.Tasks
         public const int MaxTitleLength = 256;
         public const int MaxDescriptionLength = 64 * 1024; //64KB
 
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public string Title { get; set; }
 
-        public DateTime CreationTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [StringLength(MaxDescriptionLength)]
+        public string Description { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public TaskState State { get; set; }
+
+        public Task()
+        {
+            CreationTime = Clock.Now;
+            State = TaskState.Open;
+        }
+
+        public Task(string title, string description = null)
+           : this()
+        {
+            Title = title;
+            Description = description;
+        }        
+
+        public enum TaskState : byte
+        {
+            Open = 0,
+            Completed = 1
+        }
     }
 }
