88a6b02eb655c2a058e97dff2e8ccdf7ea298b4c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-17 14:04:11 +0300

    Task entity created

---
+++ src/CompareX.Core/Tasks/Task.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CompareX.Tasks
+{
+    [Table("AppTasks")]
+    public class Task : Entity, IHasCreationTime
+    {
+        public const int MaxTitleLength = 256;
+        public const int MaxDescriptionLength = 64 * 1024; //64KB
+
+
+        public DateTime CreationTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
