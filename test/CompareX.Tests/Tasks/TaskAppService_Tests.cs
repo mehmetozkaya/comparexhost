dccb16bec2565859166abf6096b519a80a6ab863
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-19 13:07:04 +0300

    Should_Get_All_Tasks

--- test/CompareX.Tests/Tasks/TaskAppService_Tests.cs
+++ test/CompareX.Tests/Tasks/TaskAppService_Tests.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CompareX.Tasks;
+using CompareX.Tasks.Dto;
+using Shouldly;
+using Xunit;
 
 namespace CompareX.Tests.Tasks
 {
     public class TaskAppService_Tests : CompareXTestBase
     {
+        private readonly ITaskAppService _taskAppService;
+
+        public TaskAppService_Tests()
+        {
+            _taskAppService = Resolve<ITaskAppService>();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Get_All_Tasks()
+        {
+            // Act
+            var output = await _taskAppService.GetAll(new GetAllTasksInput());
+
+            // Assert
+            output.Items.Count.ShouldBe(2);             
+        }
 
     }
 }
