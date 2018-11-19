b44743ff0916ce5b8a961e0802c09bb219df0114
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-19 11:48:36 +0300

    TaskAppService_Tests

---
+++ test/CompareX.Tests/Tasks/TaskAppService_Tests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Tests.Tasks
+{
+    public class TaskAppService_Tests : CompareXTestBase
+    {
+
+    }
+}
