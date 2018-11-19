48a4eec9c8cb8c0f1934bba751be31047ace0b6c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-19 18:36:15 +0300

    ITaskRepository

---
+++ src/CompareX.Core/Tasks/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Tasks
+{
+    public interface ITaskRepository : IRepository<Task>
+    {
+        List<Task> GetAllWithPeople(Guid? assignedPersonId, TaskState? state);
+    }
+}
