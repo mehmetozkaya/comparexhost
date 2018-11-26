3acf3e89415eb4081c86950b2397e589150e541c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-26 15:42:48 +0300

    CaseRegistrationPolicy

---
+++ src/CompareX.Core/Case/CaseRegistrationPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CompareX.Authorization.Users;
+
+namespace CompareX.Case
+{
+    public class CaseRegistrationPolicy : ICaseRegistrationPolicy
+    {
+
+        public Task CheckRegistrationAttemptAsync(Case checkCase, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
