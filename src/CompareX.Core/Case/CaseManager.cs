dafa24be9b0bd255e6966632e23e742145633b8c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-26 16:21:33 +0300

    CaseManager

---
+++ src/CompareX.Core/Case/CaseManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Events.Bus;
+using CompareX.Authorization.Users;
+
+namespace CompareX.Case
+{
+    public class CaseManager : ICaseManager
+    {
+        public IEventBus EventBus { get; set; }
+
+        private readonly ICaseRegistrationPolicy _registrationPolicy;
+        private readonly IRepository<CaseRegistration> _eventRegistrationRepository;
+        private readonly IRepository<Case, Guid> _eventRepository;
+
+
+        public void Cancel(Case cancelCase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CancelRegistrationAsync(Case cancelRegisterCase, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(Case newCase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Case> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<User>> GetRegisteredUsersAsync(Case registeredCase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CaseRegistration> RegisterAsync(Case registerCase, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/CompareX.Core/Case/ICaseManager.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Services;
+using CompareX.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompareX.Case
+{
+    public interface ICaseManager : IDomainService
+    {
+        Task<Case> GetAsync(Guid id);
+        Task CreateAsync(Case newCase);
+        void Cancel(Case cancelCase);
+        Task<CaseRegistration> RegisterAsync(Case registerCase, User user);
+        Task CancelRegistrationAsync(Case cancelRegisterCase, User user);
+        Task<IReadOnlyList<User>> GetRegisteredUsersAsync(Case registeredCase);
+    }
+}
