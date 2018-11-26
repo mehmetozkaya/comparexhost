b378acd0b413837fcb93e2b6fd3e848e5b3ba207
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-26 15:40:46 +0300

    ICaseRegistrationPolicy

---
+++ src/CompareX.Core/Case/ICaseRegistrationPolicy.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Domain.Services;
+using CompareX.Authorization.Users;
+using System.Threading.Tasks;
+
+namespace CompareX.Case
+{
+    public interface ICaseRegistrationPolicy : IDomainService
+    {
+        Task CheckRegistrationAttemptAsync(Case checkCase, User user);
+    }
+}
