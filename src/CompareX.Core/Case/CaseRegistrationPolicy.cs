7fea664d2a8516110ac69eadb5d346cf67a067a0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-26 15:55:37 +0300

    CheckCaseRegistrationFrequencyAsync

--- src/CompareX.Core/Case/CaseRegistrationPolicy.cs
+++ src/CompareX.Core/Case/CaseRegistrationPolicy.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Configuration;
+using Abp.Domain.Repositories;
+using Abp.Timing;
+using Abp.UI;
 using CompareX.Authorization.Users;
+using CompareX.Configuration;
 
 namespace CompareX.Case
 {
     public class CaseRegistrationPolicy : ICaseRegistrationPolicy
     {
+        private readonly IRepository<CaseRegistration> _caseRegistrationRepository;
+        private readonly ISettingManager _settingManager;
 
-        public Task CheckRegistrationAttemptAsync(Case checkCase, User user)
+        public CaseRegistrationPolicy(IRepository<CaseRegistration> caseRegistrationRepository, ISettingManager settingManager)
         {
-            throw new NotImplementedException();
+            _caseRegistrationRepository = caseRegistrationRepository ?? throw new ArgumentNullException(nameof(caseRegistrationRepository));
+            _settingManager = settingManager ?? throw new ArgumentNullException(nameof(settingManager));
         }
+
+        public async Task CheckRegistrationAttemptAsync(Case checkCase, User user)
+        {
+            if (checkCase == null) { throw new ArgumentNullException("checkCase"); }
+            if (user == null) { throw new ArgumentNullException("user"); }
+
+            CheckCaseDate(checkCase);
+            await CheckCaseRegistrationFrequencyAsync(user);
+        }
+
+        private static void CheckCaseDate(Case checkCase)
+        {
+            if (checkCase.IsInPast())
+            {
+                throw new UserFriendlyException("Can not register event in the past!");
+            }
+        }
+
+        private async Task CheckCaseRegistrationFrequencyAsync(User user)
+        {
+            var oneMonthAgo = Clock.Now.AddDays(-30);
+            var maxAllowedEventRegistrationCountInLast30DaysPerUser = await _settingManager.GetSettingValueAsync<int>(AppSettingNames.MaxAllowedEventRegistrationCountInLast30DaysPerUser);
+
+
+        }
+
+
     }
 }
