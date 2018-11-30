353cd623fa6697114b76f78ccad54f16250661f5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-30 15:53:39 +0300

    ICaseAppService added

---
+++ src/CompareX.Application/Case/ICaseAppService.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Case
+{
+    public interface ICaseAppService : IApplicationService
+    {
+        Task<ListResultDto<EventListDto>> GetListAsync(GetEventListInput input);
+
+        Task<EventDetailOutput> GetDetailAsync(EntityDto<Guid> input);
+
+        Task CreateAsync(CreateEventInput input);
+
+        Task CancelAsync(EntityDto<Guid> input);
+
+        Task<EventRegisterOutput> RegisterAsync(EntityDto<Guid> input);
+
+        Task CancelRegistrationAsync(EntityDto<Guid> input);
+    }
+}
