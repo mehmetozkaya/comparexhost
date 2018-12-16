1a1c1af338806cc9a4ec3f3e684a58cde413d265
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-16 19:20:17 +0300

    SetFeatures

--- src/CompareX.Core/Features/CompareXFeatureProvider.cs
+++ src/CompareX.Core/Features/CompareXFeatureProvider.cs
@@ -1,7 +1,7 @@
 ﻿using Abp.Application.Features;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Abp.Localization;
+using Abp.Runtime.Validation;
+using Abp.UI.Inputs;
 
 namespace CompareX.Features
 {
@@ -9,9 +9,41 @@ namespace CompareX.Features
     {
         public override void SetFeatures(IFeatureDefinitionContext context)
         {
-            var sampleBooleanFeature = context.Create("SampleBooleanFeature", defaultValue: "false");
-            sampleBooleanFeature.CreateChildFeature("SampleNumericFeature", defaultValue: "10");
-            context.Create("SampleSelectionFeature", defaultValue: "B");            
+            //var sampleBooleanFeature = context.Create("SampleBooleanFeature", defaultValue: "false");
+            //sampleBooleanFeature.CreateChildFeature("SampleNumericFeature", defaultValue: "10");
+            //context.Create("SampleSelectionFeature", defaultValue: "B");
+
+            var sampleBooleanFeature = context.Create(
+                                       CompareXFeatures.SampleBooleanFeature,
+                                       defaultValue: "false",
+                                       displayName: L("Sample boolean feature"),
+                                       inputType: new CheckboxInputType()
+                                       );
+
+            sampleBooleanFeature.CreateChildFeature(
+                                    CompareXFeatures.SampleNumericFeature,
+                                    defaultValue: "10",
+                                    displayName: L("Sample numeric feature"),
+                                    inputType: new SingleLineStringInputType(new NumericValueValidator(1, 1000000))
+                                    );
+
+            context.Create(
+                        CompareXFeatures.SampleSelectionFeature,
+                        defaultValue: "B",
+                        displayName: L("Sample selection feature"),
+                        inputType: new ComboboxInputType(
+                            new StaticLocalizableComboboxItemSource(
+                                new LocalizableComboboxItem("A", L("Selection A")),
+                                new LocalizableComboboxItem("B", L("Selection B")),
+                                new LocalizableComboboxItem("C", L("Selection C"))
+                                )
+                            )
+                        );
+        }
+
+        private static ILocalizableString L(string name)
+        {
+            return new LocalizableString(name, CompareXConsts.LocalizationSourceName);
         }
     }
 }
---
+++ src/CompareX.Core/Features/CompareXFeatures.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Features
+{
+    public class CompareXFeatures
+    {
+        public const string SampleBooleanFeature = "SampleBooleanFeature";
+        public const string SampleNumericFeature = "SampleNumericFeature";
+        public const string SampleSelectionFeature = "SampleSelectionFeature";
+    }
+}
