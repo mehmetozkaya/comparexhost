22ed671522387d67985cd131c51fb042de58b103
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-21 21:08:16 +0300

    Course

--- src/CompareX.Core/Courses/Course.cs
+++ src/CompareX.Core/Courses/Course.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CompareX.Courses
 {
-    class Course
+    [Table("AppCourses")]
+    public class Course : FullAuditedEntity, IMustHaveTenant
     {
+        public int TenantId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
