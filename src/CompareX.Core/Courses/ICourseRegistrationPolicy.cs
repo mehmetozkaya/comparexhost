166f7afc4cb58a6876a3c496e4c69d873be94d6f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:12:34 +0300

    ICourseRegistrationPolicy

---
+++ src/CompareX.Core/Courses/ICourseRegistrationPolicy.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Services;
+using CompareX.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareX.Courses
+{
+    public interface ICourseRegistrationPolicy : IDomainService
+    {
+        Task CheckRegistrationAttemptAsync(Course checkCourse, User user);
+    }
+}
