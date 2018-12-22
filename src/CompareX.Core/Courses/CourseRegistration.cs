30774cd4a65d97dadc92c8a0c6a46b6d815e627e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:11:04 +0300

    CancelAsync of Course

--- src/CompareX.Core/Courses/CourseRegistration.cs
+++ src/CompareX.Core/Courses/CourseRegistration.cs
@@ -1,11 +1,14 @@
 ﻿using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Repositories;
+using Abp.UI;
 using CompareX.Authorization.Users;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CompareX.Courses
 {
@@ -21,6 +24,41 @@ namespace CompareX.Courses
         [ForeignKey("UserId")]
         public virtual User User { get; protected set; }
         public virtual long UserId { get; protected set; }
+
+        protected CourseRegistration()
+        {        
+        }
+
+        public static async Task<CourseRegistration> CreateAsync(Course newCourse, User user, ICourseRegistrationPolicy registrationPolicy)
+        {
+            await registrationPolicy.CheckRegistrationAttemptAsync(newCourse, user);
+
+            return new CourseRegistration
+            {
+                TenantId = newCourse.TenantId,
+                CourseId = newCourse.Id,
+                Course = newCourse,
+                UserId = user.Id,
+                User = user
+            };
+        }
+
+        public async Task CancelAsync(IRepository<CourseRegistration> repository)
+        {
+            if (repository == null) { throw new ArgumentNullException("repository"); }
+
+            if (Course.IsInPast())
+            {
+                throw new UserFriendlyException("Can not cancel event which is in the past!");
+            }
+
+            if (Course.IsAllowedCancellationTimeEnded())
+            {
+                throw new UserFriendlyException("It's too late to cancel your registration!");
+            }
+
+            await repository.DeleteAsync(this);
+        }
 
     }
 }
