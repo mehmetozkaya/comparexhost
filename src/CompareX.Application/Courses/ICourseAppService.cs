b035a45012e2218178f1f6272f00c0e98c8c313e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 14:28:22 +0300

    ICourseAppService

---
+++ src/CompareX.Application/Courses/ICourseAppService.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareX.Courses
+{
+    public interface ICourseAppService : IApplicationService
+    {
+        Task<ListResultDto<CourseDto>> GetListAsync(GetCourseInput input);
+
+        Task<CourseDetailOutput> GetDetailAsync(EntityDto input);
+
+        Task CreateAsync(CreateCourseInput input);
+
+        Task CancelAsync(EntityDto input);
+
+        Task<CourseRegisterOutput> RegisterAsync(EntityDto input);
+
+        Task CancelRegistrationAsync(EntityDto input);
+    }
+}
