b3ed3822a2103dee0c3e161bd5355a3de81c559e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:31:43 +0300

    CourseCancelledEvent

--- src/CompareX.Core/Courses/Event/CourseCancelledEvent.cs
+++ src/CompareX.Core/Courses/Event/CourseCancelledEvent.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Abp.Events.Bus.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CompareX.Courses
 {
-    public class CourseCancelledEvent
+    public class CourseCancelledEvent : EntityEventData<Course>
     {
+        public CourseCancelledEvent(Course entity) : base(entity)
+        {
+        }
     }
 }
