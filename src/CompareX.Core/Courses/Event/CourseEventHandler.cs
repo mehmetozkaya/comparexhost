e76a21e43942d95efb32070ff97451d73e0d6862
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 14:13:31 +0300

    HandleEvent 1

--- src/CompareX.Core/Courses/Event/CourseEventHandler.cs
+++ src/CompareX.Core/Courses/Event/CourseEventHandler.cs
@@ -1,9 +1,12 @@
 ﻿using Abp.Dependency;
+using Abp.Domain.Uow;
 using Abp.Events.Bus.Entities;
 using Abp.Events.Bus.Handlers;
 using Castle.Core.Logging;
+using CompareX.Authorization.Users;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CompareX.Courses
@@ -12,9 +15,31 @@ namespace CompareX.Courses
     {
         public ILogger Logger { get; set; }
 
-        public void HandleEvent(EntityCreatedEventData<Course> eventData)
+        private readonly ICourseManager _courseManager;
+        private readonly UserManager _userManager;
+
+        public CourseEventHandler(ICourseManager courseManager, UserManager userManager)
+        {            
+            _courseManager = courseManager ?? throw new ArgumentNullException(nameof(courseManager));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+
+            Logger = NullLogger.Instance;
+        }
+
+        [UnitOfWork]
+        public void HandleEvent(EntityCreatedEventData<Course> courseData)
         {
-            throw new NotImplementedException();
+            //TODO: Send email to all tenant users as a notification
+            var users = _userManager
+              .Users
+              .Where(u => u.TenantId == courseData.Entity.TenantId)
+              .ToList();
+
+            foreach (var user in users)
+            {
+                var message = string.Format("Hey! There is a new event '{0}' on {1}! Want to register?", courseData.Entity.Title, courseData.Entity.Date);
+                Logger.Debug(string.Format("TODO: Send email to {0} -> {1}", user.EmailAddress, message));
+            }            
         }
 
         public void HandleEvent(CourseDateChangedEvent eventData)
