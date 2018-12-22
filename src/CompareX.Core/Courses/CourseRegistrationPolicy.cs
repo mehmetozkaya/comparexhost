b8977ffbf6a3b1f43058e6470623a11ac16024a5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:16:15 +0300

    CheckRegistrationAttemptAsync

--- src/CompareX.Core/Courses/CourseRegistrationPolicy.cs
+++ src/CompareX.Core/Courses/CourseRegistrationPolicy.cs
@@ -1,10 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Abp.Configuration;
+using Abp.Domain.Repositories;
+using Abp.UI;
+using CompareX.Authorization.Users;
 
 namespace CompareX.Courses
 {
-    public class CourseRegistrationPolicy
+    public class CourseRegistrationPolicy : ICourseRegistrationPolicy
     {
+        private readonly IRepository<CourseRegistration> _courseRegistrationRepository;
+        private readonly ISettingManager _settingManager;
+
+        public CourseRegistrationPolicy(IRepository<CourseRegistration> courseRegistrationRepository, ISettingManager settingManager)
+        {
+            _courseRegistrationRepository = courseRegistrationRepository ?? throw new ArgumentNullException(nameof(courseRegistrationRepository));
+            _settingManager = settingManager ?? throw new ArgumentNullException(nameof(settingManager));
+        }
+
+        public Task CheckRegistrationAttemptAsync(Course checkCourse, User user)
+        {
+            if (checkCourse == null) { throw new ArgumentNullException("checkCourse"); }
+            if (user == null) { throw new ArgumentNullException("user"); }
+
+            CheckCourseDate(checkCourse);
+            await CheckCaseRegistrationFrequencyAsync(user);
+
+        }
+
+        private static void CheckCourseDate(Course checkCourse)
+        {
+            if (checkCourse.IsInPast())
+            {
+                throw new UserFriendlyException("Can not register event in the past!");
+            }
+        }
     }
 }
