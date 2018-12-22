74838e2f2bb530b8268cbe7ee4843f9f9f994b1d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 12:51:30 +0300

    Course entity filled

--- src/CompareX.Core/Courses/Course.cs
+++ src/CompareX.Core/Courses/Course.cs
@@ -1,7 +1,11 @@
 ﻿using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using Abp.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -10,6 +14,113 @@ namespace CompareX.Courses
     [Table("AppCourses")]
     public class Course : FullAuditedEntity, IMustHaveTenant
     {
-        public int TenantId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public const int MaxTitleLength = 128;
+        public const int MaxDescriptionLength = 2048;
+        public virtual int TenantId { get; set; }
+
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public virtual string Title { get; protected set; }
+
+        [StringLength(MaxDescriptionLength)]
+        public virtual string Description { get; protected set; }
+
+        public virtual DateTime Date { get; protected set; }
+
+        public virtual bool IsCancelled { get; protected set; }
+
+        [Range(0, int.MaxValue)]
+        public virtual int MaxRegistrationCount { get; protected set; }
+
+        [ForeignKey("CourseId")]
+        public virtual ICollection<CourseRegistration> Registrations { get; protected set; }
+
+        protected Course()
+        {
+        }
+
+        public static Course Create(int tenantId, string title, DateTime date, string description = null, int maxRegistrationCount = 0)
+        {
+            var newCourse = new Course
+            {                
+                TenantId = tenantId,
+                Title = title,
+                Description = description,
+                MaxRegistrationCount = maxRegistrationCount
+            };
+
+            newCourse.SetDate(date);
+            newCourse.Registrations = new Collection<CourseRegistration>();
+            return newCourse;
+        }
+
+        public bool IsInPast()
+        {
+            return Date < Clock.Now;
+        }
+
+        public bool IsAllowedCancellationTimeEnded()
+        {
+            return Date.Subtract(Clock.Now).TotalHours <= 2.0; //2 hours can be defined as Course property and determined per course
+        }
+
+        public void ChangeDate(DateTime date)
+        {
+            if (date == Date)
+            {
+                return;
+            }
+
+            SetDate(date);
+
+            DomainEvents.EventBus.Trigger(new CourseDateChangedEvent(this));
+        }
+
+        internal void Cancel()
+        {
+            AssertNotInPast();
+            IsCancelled = true;
+        }
+
+        internal void Delete()
+        {
+            IsCancelled = true;
+        }
+
+        private void SetDate(DateTime date)
+        {
+            AssertNotCancelled();
+
+            if (date < Clock.Now)
+            {
+                throw new UserFriendlyException("Can not set an course's date in the past!");
+            }
+
+            if (date <= Clock.Now.AddHours(3)) //3 can be configurable per tenant
+            {
+                throw new UserFriendlyException("Should set an course's date 3 hours before at least!");
+            }
+
+            Date = date;
+
+            DomainEvents.EventBus.Trigger(new CourseDateChangedEvent(this));
+        }
+
+        private void AssertNotInPast()
+        {
+            if (IsInPast())
+            {
+                throw new UserFriendlyException("This event was in the past");
+            }
+        }
+
+        private void AssertNotCancelled()
+        {
+            if (IsCancelled)
+            {
+                throw new UserFriendlyException("This event is canceled!");
+            }
+        }
+
     }
 }
