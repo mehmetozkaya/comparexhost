fda8a40ed7bf3b714fa7c4431ae7ee3efc861aea
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 14:31:17 +0300

    CourseDto

---
+++ src/CompareX.Application/Courses/Dto/CourseDtoCollection.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Courses.Dto
+{
+    public class CourseDtoCollection
+    {
+        [AutoMapFrom(typeof(Course))]
+        public class CourseDto : FullAuditedEntityDto
+        {
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+
+            public DateTime Date { get; set; }
+
+            public bool IsCancelled { get; set; }
+
+            public virtual int MaxRegistrationCount { get; protected set; }
+
+            public int RegistrationsCount { get; set; }
+        }
+    }
+}
