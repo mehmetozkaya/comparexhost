6b9934ec1c061c9a077bbcf496dc77e345ac6815
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:54:09 +0300

    ICourseManager Get-Create-Cancel-Register

--- src/CompareX.Core/Courses/Event/CourseEventHandler.cs
+++ src/CompareX.Core/Courses/Event/CourseEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Dependency;
 using Abp.Events.Bus.Entities;
 using Abp.Events.Bus.Handlers;
+using Castle.Core.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,8 @@ namespace CompareX.Courses
 {
     public class CourseEventHandler : IEventHandler<EntityCreatedEventData<Course>>, IEventHandler<CourseDateChangedEvent>, IEventHandler<CourseCancelledEvent>, ITransientDependency
     {
+        public ILogger Logger { get; set; }
+
         public void HandleEvent(EntityCreatedEventData<Course> eventData)
         {
             throw new NotImplementedException();
---
+++ src/CompareX.Core/Courses/Manager/ICourseManager.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Domain.Services;
+using CompareX.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareX.Courses.Manager
+{
+    public interface ICourseManager : IDomainService
+    {
+        Task<Course> GetAsync(int id);
+        Task CreateAsync(Course newCase);
+        void Cancel(Course cancelCase);
+        Task<CourseRegistration> RegisterAsync(Course registerCase, User user);
+        Task CancelRegistrationAsync(Course cancelRegisterCase, User user);
+        Task<IReadOnlyList<User>> GetRegisteredUsersAsync(Course registeredCase);
+    }
+}
