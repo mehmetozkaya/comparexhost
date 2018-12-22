482e9b42b8feb0981b2cc3668f54163a2ab0e101
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:51:22 +0300

    CourseEventHandler added

---
+++ src/CompareX.Core/Courses/Event/CourseEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Dependency;
+using Abp.Events.Bus.Entities;
+using Abp.Events.Bus.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Courses
+{
+    public class CourseEventHandler : IEventHandler<EntityCreatedEventData<Course>>, IEventHandler<CourseDateChangedEvent>, IEventHandler<CourseCancelledEvent>, ITransientDependency
+    {
+    }
+}
---
+++ src/CompareX.Core/Courses/Manager/CourseManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareX.Courses
+{
+    public class CourseManager
+    {
+
+    }
+}
