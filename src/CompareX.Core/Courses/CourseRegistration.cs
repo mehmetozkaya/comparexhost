11a61e097f43eb7b9fe9a8c194fd5e79ec989b64
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-22 13:06:18 +0300

    MaxTitleLength

--- src/CompareX.Core/Courses/CourseRegistration.cs
+++ src/CompareX.Core/Courses/CourseRegistration.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CompareX.Courses
 {
     [Table("AppCourseRegistrations")]
-    public class CourseRegistration
+    public class CourseRegistration : FullAuditedEntity, IMustHaveTenant
     {
+        public const int MaxTitleLength = 128;
+        public const int MaxDescriptionLength = 2048;
+
+        public virtual int TenantId { get; set; }
+
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public virtual string Title { get; protected set; }
+
+        [StringLength(MaxDescriptionLength)]
+        public virtual string Description { get; protected set; }
+
+
+
+
     }
 }
